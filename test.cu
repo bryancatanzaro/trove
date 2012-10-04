3cf00f4a0f374cc18b143db575cbd46a852f98f3
Bryan Catanzaro <bcatanzaro@acm.org>
2012-10-03 21:09:36 -0700

    Test all functional arities.

--- test.cu
+++ test.cu
@@ -25,7 +25,7 @@ __global__ void test_transpose_indices(Value* r) {
 }
 
 template<int size, typename T>
-__global__ void test_transpose(T* r) {
+__global__ void test_c2r_transpose(T* r) {
     typedef typename homogeneous_tuple<size, T>::type Value;
     typedef typename homogeneous_tuple<size, int>::type Indices;
     int global_index = threadIdx.x + blockDim.x * blockIdx.x;
@@ -68,7 +68,7 @@ __global__ void test_uncoalesced_store(T* r) {
 
 
 template<int size, typename T>
-__global__ void test_shared_transpose(T* r) {
+__global__ void test_shared_c2r_transpose(T* r) {
     typedef typename homogeneous_tuple<size, T>::type Value;
 
     int global_index = threadIdx.x + blockDim.x * blockIdx.x;
@@ -97,48 +97,6 @@ __global__ void test_shared_transpose(T* r) {
 }
 
 
-template<typename Tuple>
-struct tuple_sum_impl {};
-
-template<typename HT, typename TT>
-struct tuple_sum_impl<thrust::detail::cons<HT, TT> > {
-    __host__ __device__ static HT
-    impl(const thrust::detail::cons<HT, TT>& t, HT p) {
-        return tuple_sum_impl<TT>::impl(t.get_tail(), p + t.get_head());
-    }
-};
-
-template<>
-struct tuple_sum_impl<thrust::null_type> {
-    template<typename T>
-    __host__ __device__ static T impl(thrust::null_type, const T& p) {
-        return p;
-    }
-};
-
-template<typename Tuple>
-__host__ __device__
-typename thrust::tuple_element<0, Tuple>::type tuple_sum(const Tuple& t) {
-    return tuple_sum_impl<typename cons_type<Tuple>::type>::impl(t, 0);
-}
-
-template<typename Key, typename Value>
-__global__ void test_oet(
-    Key k, Value v,
-    typename thrust::tuple_element<0, Value>::type* r) {
-    int global_index = threadIdx.x + blockIdx.x * blockDim.x;
-    oet_sort_by_key(k, v);
-    r[global_index] = tuple_sum(v);
-}
-
-template<typename Key, typename Value>
-__global__ void test_bubble(
-    Key k, Value v,
-    typename thrust::tuple_element<0, Value>::type* r) {
-    int global_index = threadIdx.x + blockIdx.x * blockDim.x;
-    bubble_sort_by_key(k, v);
-    r[global_index] = tuple_sum(v);
-}
 
 template<typename T>
 void verify(thrust::device_vector<T>& d_r) {
@@ -155,105 +113,48 @@ void verify(thrust::device_vector<T>& d_r) {
     }
 }
 
-#define ARITY 8
+template<int i, typename Tail=thrust::null_type>
+struct int_cons {
+    static const int head = i;
+    typedef Tail tail;
+};
 
-int main() {
+typedef
+int_cons<2,
+    int_cons<3,
+    int_cons<4,
+    int_cons<5,
+    int_cons<7,
+    int_cons<8,
+    int_cons<9,
+    thrust::null_type> > > > > > > c2r_arities;
 
-    typedef typename homogeneous_tuple<5, int>::type five_int;
-    // five_int c = counting_tuple<five_int>::impl(15);
-    // print_tuple(c);
-    
-    // typedef thrust::tuple<int, int, int, int, int,
-    //                       int, int, int, int, int> ten_int;
-    // ten_int x = thrust::make_tuple(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-    // thrust::device_vector<ten_int> d(1);
-    // test<<<1,1>>>(x, 100, 7, thrust::raw_pointer_cast(d.data()));
-    // ten_int y = d[0];
-    // print_tuple(y);
-
-
+template<int i>
+void test_c2r_transpose() {
     int n_blocks = 15 * 8 * 100;
-    //int n_blocks = 1;
     int block_size = 256;
-    //it block_size = 32;
-    thrust::device_vector<int> e(n_blocks*block_size*ARITY);
+    thrust::device_vector<int> e(n_blocks*block_size*i);
+    test_c2r_transpose<i>
+        <<<n_blocks, block_size>>>(thrust::raw_pointer_cast(e.data()));
+    verify(e); 
+}
 
-    // typedef typename homogeneous_tuple<ARITY, int>::type n_int;
-    // thrust::device_vector<n_int> f(64);
-    // test_transpose_indices<<<1, 64>>>(
-    //     thrust::raw_pointer_cast(f.data()));
-    // for(int i = 0; i < 64; i++) {
-    //     n_int z = f[i];
-    //     std::cout << i << ": ";
-    //     print_tuple(z);
-    // }
+template<typename Cons>
+struct test_c2r_transposes {
+    static void impl() {
+        std::cout << "Testing c2r transpose for " << Cons::head <<
+            " elements per thread" << std::endl;
+        test_c2r_transpose<Cons::head>();
+        test_c2r_transposes<typename Cons::tail>::impl();
+    }
+};
 
-
-    test_transpose<ARITY><<<n_blocks, block_size>>>(
-        thrust::raw_pointer_cast(e.data()));
-    verify(e);
-    thrust::fill(e.begin(), e.end(), 0);
-    test_uncoalesced_store<ARITY><<<n_blocks, block_size>>>(
-        thrust::raw_pointer_cast(e.data()));
-    verify(e);
-    thrust::fill(e.begin(), e.end(), 0);
-    test_shared_transpose<ARITY><<<n_blocks, block_size,
-        sizeof(int) * ARITY * block_size>>>(
-            thrust::raw_pointer_cast(e.data()));
-    verify(e);
-    // thrust::fill(e.begin(), e.end(), 0);
-
-    // five_int five_k = thrust::make_tuple(9,8,7,6,5);
-    // five_int five_v = thrust::make_tuple(0,1,2,3,4);
-    // five_int five_k_s = five_k;
-    // five_int five_v_s = five_v;
-    // oet_sort_by_key(five_k_s, five_v_s);
-    // print_tuple(five_k_s);
-    // print_tuple(five_v_s);
-    // five_k_s = five_k;
-    // five_v_s = five_v;
-    // bubble_sort_by_key(five_k_s, five_v_s);
-    // print_tuple(five_k_s);
-    // print_tuple(five_v_s);
- 
-    // thrust::device_vector<int> r(n_blocks * block_size);
-    // int iterations = 1;
-    // cudaEvent_t start,stop;
-    // float time=0;
-    // cudaEventCreate(&start);
-    // cudaEventCreate(&stop);
-    // cudaEventRecord(start, 0);
-    // for (int i = 0; i < iterations; i++) {
-    //     test_oet<<<n_blocks, block_size>>>(five_k,
-    //                                        five_v,
-    //                                        thrust::raw_pointer_cast(r.data()));
-    // }
-    
-    // cudaEventRecord(stop, 0);
-    // cudaEventSynchronize(stop);
-    // cudaEventElapsedTime(&time, start, stop);
-    // time = time / (float)iterations;
-    // std::cout << "  Time: " << time << " ms" << std::endl;
-    // float kps = (float)n_blocks * block_size * 5  / (time*1000);
-    // std::cout << "  Throughput: " << kps << " Mkeys/s" << std::endl
-    //           << std::endl;
-
-    // cudaEventRecord(start, 0);
-    // for(int i = 0; i < iterations; i++) {
-    //     test_bubble<<<n_blocks, block_size>>>(five_k,
-    //                                           five_v,
-    //                                           thrust::raw_pointer_cast(r.data()));
-    // }
-    
-    // cudaEventRecord(stop, 0);
-    // cudaEventSynchronize(stop);
-    // cudaEventElapsedTime(&time, start, stop);
-    // time = time / (float)iterations;
-    // std::cout << "  Time: " << time << " ms" << std::endl;
-    // kps = (float)n_blocks * block_size * 5  / (time*1000);
-    // std::cout << "  Throughput: " << kps << " Mkeys/s" << std::endl
-    //           << std::endl;
-
-    
+template<>
+struct test_c2r_transposes<thrust::null_type> {
+    static void impl() {}
+};
+  
+int main() {
+    test_c2r_transposes<c2r_arities>::impl();  
 }
     
