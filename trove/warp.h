f58f212df14050c0139bc6436676511c576d1e5b
Sam Mish <samuelpmish@gmail.com>
2020-07-10 11:19:12 -0700

    explicitly define underlying enum type

--- trove/warp.h
+++ trove/warp.h
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace trove {
 
-enum {
+enum : unsigned {
     WARP_SIZE = 32,
     WARP_MASK = 0x1f,
     WARP_CONVERGED = 0xFFFFFFFF,
